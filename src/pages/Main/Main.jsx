117ae0a47ee51349dc73bf25a031e5854c916c0f
DanielNahshonov <daniel.n@circ.zone>
2024-10-09 14:45:03 +0300

    added table

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,4 +1,5 @@
 import Header from "../../components/Header/Header";
+import ProductList from "../../components/Table/ProductList";
 
 function Main() {
     return (
@@ -7,6 +8,7 @@ function Main() {
             <div className="lg:w-full flex flex-col lg:gap-8 lg:px-40 lg:py-4">
                 <h1 className="text-center">Main page</h1>
             </div>
+            <ProductList />
         </>
     );
 }
